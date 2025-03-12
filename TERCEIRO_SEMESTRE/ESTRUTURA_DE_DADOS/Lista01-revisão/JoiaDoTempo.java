cd29f5d59e0e5972cb392fd79e33ff4fdae18bba
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-03-12 17:09:03 -0300

    Update JoiaDoTempo.java

--- TERCEIRO_SEMESTRE/ESTRUTURA_DE_DADOS/Lista01-revisão/JoiaDoTempo.java
+++ TERCEIRO_SEMESTRE/ESTRUTURA_DE_DADOS/Lista01-revisão/JoiaDoTempo.java
@@ -5,24 +5,27 @@ public class JoiaDoTempo {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
+        boolean vitoria = false;
 
         while (true) {
 
             String HF = sc.next();
-
             String HDF = sc.next();
-
             String HA = sc.next();
 
             if (HF.equals("FIM") && HDF.equals("FIM") && HA.equals("FIM")) {
                 break;
-            }
+            } 
 
             if (HF.equals("NAO") && HDF.equals("SIM") && HA.equals("NAO")) {
-                System.out.println("VITORIA");
-            } else {
-                System.out.println("DERROTA");
+                vitoria = true;
             }
+        }
+
+        if (vitoria) {
+            System.out.println("VITORIA");
+        } else {
+            System.out.println("DERROTA");
         }
 
     }
