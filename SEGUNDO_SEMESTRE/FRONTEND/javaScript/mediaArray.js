4dac6b7a97cd78ff10ea633b954d0b85251f8500
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-04-30 20:55:55 -0300

    exercicios js

--- SEGUNDO_SEMESTRE/FRONTEND/javaScript/mediaArray.js
+++ SEGUNDO_SEMESTRE/FRONTEND/javaScript/mediaArray.js
@@ -1,1 +1,11 @@
 const numbers = [10, 9, 3, 11, 58, 16, 102, 8, 30, 21];
+
+let soma = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    soma += numbers[i];
+}
+
+let media = soma / numbers.length;
+
+console.log(`A media dos elementos do array é ${media}`)
